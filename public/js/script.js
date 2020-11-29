32c524c65097201c9f3e357d9613be201ef53241
sumanto pal <sumantopal07@gmail.com>
2020-11-29 13:53:28 +0530

    [NOT WORKING] Used ball as separate entity
    This method did'nt worked, only to be kept
    as a reference

--- public/js/script.js
+++ public/js/script.js
@@ -9,14 +9,14 @@ var ballSpeedY = 4;
 
 var player1Score = 0;
 var player2Score = 0;
-const WINNING_SCORE = 3;
+var WINNING_SCORE = 3;
 
 var showingWinScreen = false;
 
 var paddle1Y = 250;
 var paddle2Y = 250;
-const PADDLE_THICKNESS = 10;
-const PADDLE_HEIGHT = 100;
+var PADDLE_THICKNESS = 10;
+var PADDLE_HEIGHT = 100;
 
 function calculateMousePos(evt) {
     var rect = canvas.getBoundingClientRect();
@@ -36,146 +36,110 @@ function handleMouseClick(evt) {
         showingWinScreen = false;
     }
 }
+function computerMovement() {
+    socket.on('opponent', data => {
+        paddle2Y = data;
+    })
+}
 
 window.onload = function () {
-    canvas = document.getElementById('gameCanvas');
-    canvasContext = canvas.getContext('2d');
+            canvas = document.getElementById('gameCanvas');
+            canvasContext = canvas.getContext('2d');
 
-    var framesPerSecond = 30;
-    setInterval(function () {
-        moveEverything();
-        drawEverything();
-    }, 1000 / framesPerSecond);
+            var framesPerSecond = 30;
+            setInterval(function () {
+                socket.emit('me', paddle1Y);
+                computerMovement();
+                if (!showingWinScreen) {
+                    let XXX = {
+                        ballX: ballX,
+                        ballY: ballY,
+                        ballSpeedX: ballSpeedX,
+                        ballSpeedY: ballSpeedY,
+                        paddle1Y: paddle1Y,
+                        paddle2Y: paddle2Y,
+                        PADDLE_HEIGHT: PADDLE_HEIGHT,
+                        player1Score: player1Score,
+                        player2Score: player2Score,
+                        WINNING_SCORE: WINNING_SCORE,
+                        WIDTH: canvas.width,
+                        HEIGHT: canvas.height,
+                        showingWinScreen: showingWinScreen
+                    };
+                    socket.emit('moveEverything', XXX);
+                    socket.on('recieve', data => {
+                        ballX = data.ballX
+                        ballY = data.ballY
+                        ballSpeedX = data.ballSpeedX
+                        ballSpeedY = data.ballSpeedY
+                        paddle1Y = data.paddle1Y
+                        paddle2Y = data.paddle2Y
+                        player1Score = data.player1Score
+                        player2Score = data.player2Score
+                        WINNING_SCORE = data.WINNING_SCORE
+                        showingWinScreen = data.showingWinScreen
+                    });
+                }
+                drawEverything();
+            }, 1000 / framesPerSecond);
 
-    canvas.addEventListener('mousedown', handleMouseClick);
+            canvas.addEventListener('mousedown', handleMouseClick);
 
-    canvas.addEventListener('mousemove',
-        function (evt) {
-            var mousePos = calculateMousePos(evt);
-            paddle1Y = mousePos.y - (PADDLE_HEIGHT / 2);
-        });
-}
-
-function ballReset() {
-    if (player1Score >= WINNING_SCORE ||
-        player2Score >= WINNING_SCORE) {
-
-        showingWinScreen = true;
-
-    }
-
-    ballSpeedX = -ballSpeedX;
-    ballX = canvas.width / 2;
-    ballY = canvas.height / 2;
-}
-
-function computerMovement() {
-    setInterval(function(){
-        socket.on('opponent',data=>{
-            paddle2Y=data;
-        });
-    }, 1000 / 30);
-    // var paddle2YCenter = paddle2Y + (PADDLE_HEIGHT / 2);
-    // if (paddle2YCenter < ballY - 35) {
-    //     paddle2Y = paddle2Y + 6;
-    // } else if (paddle2YCenter > ballY + 35) {
-    //     paddle2Y = paddle2Y - 6;
-    // }
-}
-
-function moveEverything() {
-    if (showingWinScreen) {
-        return;
-    }
-
-    computerMovement();
-
-    ballX = ballX + ballSpeedX;
-    ballY = ballY + ballSpeedY;
-
-    if (ballX < 0) {
-        if (ballY > paddle1Y &&
-            ballY < paddle1Y + PADDLE_HEIGHT) {
-            ballSpeedX = -ballSpeedX;
-
-            var deltaY = ballY
-                - (paddle1Y + PADDLE_HEIGHT / 2);
-            ballSpeedY = deltaY * 0.35;
-        } else {
-            player2Score++; // must be BEFORE ballReset()
-            ballReset();
+            canvas.addEventListener('mousemove',
+                function (evt) {
+                    var mousePos = calculateMousePos(evt);
+                    paddle1Y = mousePos.y - (PADDLE_HEIGHT / 2);
+                });
         }
-    }
-    if (ballX > canvas.width) {
-        if (ballY > paddle2Y &&
-            ballY < paddle2Y + PADDLE_HEIGHT) {
-            ballSpeedX = -ballSpeedX;
 
-            var deltaY = ballY
-                - (paddle2Y + PADDLE_HEIGHT / 2);
-            ballSpeedY = deltaY * 0.35;
-        } else {
-            player1Score++; // must be BEFORE ballReset()
-            ballReset();
-        }
-    }
-    if (ballY < 0) {
-        ballSpeedY = -ballSpeedY;
-    }
-    if (ballY > canvas.height) {
-        ballSpeedY = -ballSpeedY;
-    }
-}
+
 
 function drawNet() {
-    for (var i = 0; i < canvas.height; i += 40) {
-        colorRect(canvas.width / 2 - 1, i, 2, 20, 'white');
-    }
-}
-
-function drawEverything() {
-    // next line blanks out the screen with black
-    colorRect(0, 0, canvas.width, canvas.height, 'black');
-
-    if (showingWinScreen) {
-        canvasContext.fillStyle = 'white';
-
-        if (player1Score >= WINNING_SCORE) {
-            canvasContext.fillText("Left Player Won", 350, 200);
-        } else if (player2Score >= WINNING_SCORE) {
-            canvasContext.fillText("Right Player Won", 350, 200);
+            for (var i = 0; i < canvas.height; i += 40) {
+                colorRect(canvas.width / 2 - 1, i, 2, 20, 'white');
+            }
         }
 
-        canvasContext.fillText("click to continue", 350, 500);
-        return;
-    }
+function drawEverything() {
+            // next line blanks out the screen with black
+            colorRect(0, 0, canvas.width, canvas.height, 'black');
 
-    drawNet();
+            if (showingWinScreen) {
+                canvasContext.fillStyle = 'white';
 
-    // this is left player paddle
-    colorRect(0, paddle1Y, PADDLE_THICKNESS, PADDLE_HEIGHT, 'white');
+                if (player1Score >= WINNING_SCORE) {
+                    canvasContext.fillText("Left Player Won", 350, 200);
+                } else if (player2Score >= WINNING_SCORE) {
+                    canvasContext.fillText("Right Player Won", 350, 200);
+                }
 
-    // this is right computer paddle
-    colorRect(canvas.width - PADDLE_THICKNESS, paddle2Y, PADDLE_THICKNESS, PADDLE_HEIGHT, 'white');
+                canvasContext.fillText("click to continue", 350, 500);
+                return;
+            }
 
-    // next line draws the ball
-    colorCircle(ballX, ballY, 10, 'white');
+            drawNet();
 
-    canvasContext.fillText(player1Score, 100, 100);
-    canvasContext.fillText(player2Score, canvas.width - 100, 100);
-}
+            // this is left player paddle
+            colorRect(0, paddle1Y, PADDLE_THICKNESS, PADDLE_HEIGHT, 'white');
+
+            // this is right computer paddle
+            colorRect(canvas.width - PADDLE_THICKNESS, paddle2Y, PADDLE_THICKNESS, PADDLE_HEIGHT, 'white');
+
+            // next line draws the ball
+            colorCircle(ballX, ballY, 10, 'white');
+
+            canvasContext.fillText(player1Score, 100, 100);
+            canvasContext.fillText(player2Score, canvas.width - 100, 100);
+        }
 
 function colorCircle(centerX, centerY, radius, drawColor) {
-    canvasContext.fillStyle = drawColor;
-    canvasContext.beginPath();
-    canvasContext.arc(centerX, centerY, radius, 0, Math.PI * 2, true);
-    canvasContext.fill();
-}
+            canvasContext.fillStyle = drawColor;
+            canvasContext.beginPath();
+            canvasContext.arc(centerX, centerY, radius, 0, Math.PI * 2, true);
+            canvasContext.fill();
+        }
 
 function colorRect(leftX, topY, width, height, drawColor) {
-    canvasContext.fillStyle = drawColor;
-    canvasContext.fillRect(leftX, topY, width, height);
-}
-setInterval(function(){
-    socket.emit('me',paddle1Y);
-}, 1000 / 30);
+            canvasContext.fillStyle = drawColor;
+            canvasContext.fillRect(leftX, topY, width, height);
+        }
--- src/index.js
+++ src/index.js
@@ -10,15 +10,123 @@ app.set('view engine', 'ejs')
 
 const port = process.env.PORT || 5000
 const publicDirectoryPath = path.join(__dirname, '../public')
-
+app.use(express.json({limit: '1000mb'}));
 app.use(express.static(publicDirectoryPath))
-
+let ballX=null;
+let ballY=null;
+let ballSpeedX=null;
+let ballSpeedY=null;
+let paddle1Y=null;
+let paddle2Y=null;
+let PADDLE_HEIGHT=null;
+let player1Score=null;
+let player2Score=null;
+let WINNING_SCORE=null;
+let height=null;
+let width=null;
+let showingWinScreen=null;
 
 io.on('connection', (socket) => {
     console.log('New WebSocket connection');
-    socket.on('me',data=>{
-        socket.broadcast.emit('opponent',data);
+    socket.on('me', dataP => {
+        socket.broadcast.emit('opponent', dataP);
     });
+    socket.on('moveEverything', data => {
+        if (ballX == null
+        || ballY == null
+        || ballSpeedX == null
+        || ballSpeedX == null
+        || paddle1Y == null
+        || paddle2Y == null
+        || PADDLE_HEIGHT == null
+        || player1Score == null
+        || player2Score == null
+        || WINNING_SCORE == null
+        || width == null
+        || height == null
+        || showingWinScreen == null
+        ) {
+            ballX = data.ballX
+            ballY = data.ballY
+            ballSpeedX = data.ballSpeedX
+            ballSpeedX = data.ballSpeedY
+            paddle1Y = data.paddle1Y
+            paddle2Y = data.paddle2Y
+            PADDLE_HEIGHT = data.PADDLE_HEIGHT
+            player1Score = data.player1Score
+            player2Score = data.player2Score
+            WINNING_SCORE = data.WINNING_SCORE
+            width = data.WIDTH
+            height = data.HEIGHT
+            showingWinScreen = data.showingWinScreen
+        }
+        ballX = ballX + ballSpeedX;
+        ballY = ballY + ballSpeedY;
+
+
+        if (ballX < 0) {
+            if (ballY > paddle1Y &&
+                ballY < paddle1Y + PADDLE_HEIGHT) {
+                ballSpeedX = -ballSpeedX;
+
+                var deltaY = ballY
+                    - (paddle1Y + PADDLE_HEIGHT / 2);
+                ballSpeedY = deltaY * 0.35;
+            } else {
+                player2Score++; // must be BEFORE ballReset()
+                if (player1Score >= WINNING_SCORE ||
+                    player2Score >= WINNING_SCORE) {
+            
+                    showingWinScreen = true;
+            
+                }
+                ballSpeedX = -ballSpeedX;
+                ballX = width / 2;
+                ballY = height / 2;
+            }
+        }
+        if (ballX > width) {
+            if (ballY > paddle2Y &&
+                ballY < paddle2Y + PADDLE_HEIGHT) {
+                ballSpeedX = -ballSpeedX;
+
+                var deltaY = ballY
+                    - (paddle2Y + PADDLE_HEIGHT / 2);
+                ballSpeedY = deltaY * 0.35;
+            } else {
+                player1Score++; // must be BEFORE ballReset()
+                if (player1Score >= WINNING_SCORE ||
+                    player2Score >= WINNING_SCORE) {
+            
+                    showingWinScreen = true;
+            
+                }
+                ballSpeedX = -ballSpeedX;
+                ballX = width / 2;
+                ballY = height / 2;
+            }
+
+        }
+
+        if (ballY < 0) {
+            ballSpeedY = -ballSpeedY;
+        }
+        if (ballY > height) {
+            ballSpeedY = -ballSpeedY;
+        }
+        let data1={};
+        data1.ballX = ballX
+        data1.ballY = ballY
+        data1.ballSpeedX = ballSpeedX
+        data1.ballSpeedY = ballSpeedY
+        data1.paddle1Y = paddle1Y
+        data1.paddle2Y = paddle2Y
+        data1.player1Score = player1Score
+        data1.player2Score = player2Score
+        data1.showingWinScreen = showingWinScreen
+        socket.emit('recieve', data1);
+    });
+
 })
 
 server.listen(port, () => {
